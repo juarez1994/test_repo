ef432b04367f249b86e04c9834dc7b3ca90eecf6
juarez1994 <122844980+juarez1994@users.noreply.github.com>
2023-01-17 03:43:21 +0100

    Delete exercise2.py

--- exercise2.py
+++
@@ -1,8 +0,0 @@
-#2.Create a function that reverses a string. (i.e. Apple will result in elppA).
-
-def reverser(text):
-    reversedtext=text[::-1]
-    print(reversedtext)
-
-#test
-reverser("Apple")
